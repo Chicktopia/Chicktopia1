865fffa32504d74160b917f26419e2ed230fc153
Ticket Tycoon Developer <user@example.com>
2025-06-16 14:05:11 +0800

    Configure project for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/chicktopia1/', // This MUST match your repository name!
   plugins: [react()],
 })
